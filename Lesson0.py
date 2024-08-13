f2a79a69fcb21b0db288450c15d9a741cbb16db7
Oleg121609 <xotab_oleg@mail.ru>
2024-08-13 20:01:07 +0300

    Add files via upload

---
+++ Lesson0.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi
+print("Hello world")
+# "1st program"
+print(9 ** 0.5 * 5)
+# "2nd program"
+print(9.99 > 9.98, 1000 != 1000.1)
+# "3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(6==8)
+# "4th program"
+print(float(str("123.456")))
+print(123.456*10)
+print(int(1234.56 % 10))
+
+
+
+
