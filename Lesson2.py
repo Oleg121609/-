bab0745b45ac27546519badce772875376e03b7c
Oleg121609 <xotab_oleg@mail.ru>
2024-08-15 20:54:32 +0300

    Практическое задание по теме "Переменные".

---
+++ Lesson2.py
@@ -0,0 +1,5 @@
+NumberOfCompletedRemoteSensing = "12"
+NumberOfHoursSpent = "1.5"
+Course_name = "Python"
+Time_per_task = "0,125"
+print("Курс",Course_name,"всего задач",NumberOfCompletedRemoteSensing,"затрачено часов",NumberOfHoursSpent,"среднее время 
