191319f4e026ca6815492ea40d12f7c46e47fbb6
Oleg121609 <xotab_oleg@mail.ru>
2024-08-15 23:59:28 +0300

    Update Lesson2.py

--- Lesson2.py
+++ Lesson2.py
@@ -1,5 +1,5 @@
-NumberOfCompletedRemoteSensing = "12"
-NumberOfHoursSpent = "1.5"
+NumberOfCompletedRemoteSensing = 12
+NumberOfHoursSpent = 1.5
 Course_name = "Python"
-Time_per_task = "0,125"
-print("Курс:",Course_name,"всего задач:",NumberOfCompletedRemoteSensing,"затрачено часов:",NumberOfHoursSpent,"среднее время:",Time_per_task)
+Time_per_task = NumberOfHoursSpent / NumberOfCompletedRemoteSensing
+print("Курс:",Course_name,",всего задач:",NumberOfCompletedRemoteSensing,",затрачено часов:",NumberOfHoursSpent,",среднее время выполнения",Time_per_task,"часа")
