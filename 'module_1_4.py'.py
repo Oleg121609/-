7cf0ecb7609a575e49b0460370ca49bdc0e2cc4c
Oleg121609 <xotab_oleg@mail.ru>
2024-08-17 15:43:14 +0300

    Add files via upload

---
+++ 'module_1_4.py'.py
@@ -0,0 +1,9 @@
+my_string = input("Введите пароль: ")
+print("Пароль,", my_string)
+print("Введите пароль".upper())
+print("Введите пароль".lower())
+print("Введите пароль".replace(' ','#'))
+print(my_string [0])
+print(my_string [-1])
+
+
